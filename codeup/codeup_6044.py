d652eab93cf23a04a65e4eda52e9e0a05933d816
woocheolll <woocheol2868@gmail.com>
2022-07-13 16:20:30 +0900

    codeup 100제

---
+++ codeup/codeup_6044.py
@@ -0,0 +1,16 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+sum = a + b
+sub = a - b
+mul = a * b
+m = a // b
+n = a % b
+div = a / b
+
+print(sum)
+print(sub)
+print(mul)
+print(m)
+print(n)
+print(format(div, ".2f"))
