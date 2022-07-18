269bf210f936962558eff1735237dd730e897b74
woocheolll <woocheol2868@gmail.com>
2022-07-18 15:27:51 +0900

    실습

--- Python_실습/test_19.py
+++ Python_실습/test_19.py
@@ -1,7 +1,13 @@
 # 양의 정수 number가 주어질 때, 숫자의 길이를 구하시오. 
 # 양의 정수number를 문자열로 변경하는 것은 금지입니다. str() 사용 금지
-number = ('123')
-count = 0
-for i in number:
-    count += 1
+import math
+
+n = 123
+
+if n > 0:
+    count = int(math.log10(n))+1
+elif n == 0:
+    count = 1
+elif n < 0:
+    count = int(math.log10(-n))+2
 print(count)
