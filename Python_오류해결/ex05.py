83a384fe92b782ee38f4035b819b7e3ad8966c68
woocheolll <woocheol2868@gmail.com>
2022-07-18 14:54:10 +0900

    python 오류해결ex

---
+++ Python_오류해결/ex05.py
@@ -0,0 +1,5 @@
+number = str(22020718)
+
+print(len(number))
+
+# 타입을 str로 변경
---
+++ Python_오류해결/ex08.py
@@ -0,0 +1,11 @@
+word = "HappyHacking"
+
+count = 0
+
+for char in word:
+    if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
+        count += 1
+
+print(count)
+
+## 사이에 or 사이에 char == 추가
