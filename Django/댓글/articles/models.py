1a1ae53d56db94527c7ad69bf61ef20352b70bc3
woocheolll <woocheol2868@gmail.com>
2022-10-18 16:12:46 +0900

    댓글

---
+++ Django/댓글/articles/models.py
@@ -0,0 +1,18 @@
+from operator import mod
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/',blank=True)
+    
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    
---
+++ Django/댓글/articles/views.py
@@ -0,0 +1,75 @@
+from symbol import arith_expr
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+import accounts
+from .models import Article, Comment
+from .form import ArticleForm,CommentForm
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-pk')
+    context={
+        'articles' : articles
+    }
+    return render(request,'articles/index.html',context)
+
+# def new(request):
+#     article_form = ArticleForm()
+#     context = {
+#         'article_form' : article_form
+#     }
+#     return render(request,'articles/new.html', context=context)
+@login_required
+def create(request):
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, request.FILES)
+        if article_form.is_valid():
+            article_form.save()
+            messages.success(request, '글 작성이 완료되었습니다.')
+            return redirect('articles:index')
+    else: 
+        article_form = ArticleForm()
+    context = {
+        'article_form': article_form
+    }
+    return render(request, 'articles/form.html', context=context)
+
+def detail(request, pk):
+    # 특정 글을 가져온다.
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm()
+    # template에 객체 전달
+    context = {
+        'article': article,
+        'comments': article.comment_set.all(),
+        'comment_form': comment_form,
+    }
+    return render(request, 'articles/detail.html', context)
+
+@login_required
+def update(request,pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        article_form = ArticleForm(instance=article)
+    context = {
+        'article_form' : article_form
+    }
+    return render(request,'articles/form.html', context)
+
+def delete(request, pk):
+    Article.objects.get(pk=pk).delete()
+    return redirect('articles:index')
+
+def comment_create(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+    return redirect('articles:detail', article.pk)
