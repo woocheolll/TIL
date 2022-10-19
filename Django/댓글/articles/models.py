87bd2819437349f29bbf787288fe1c537dc6ab15
woocheolll <woocheol2868@gmail.com>
2022-10-19 23:10:49 +0900

    댓글

--- Django/댓글/articles/models.py
+++ Django/댓글/articles/models.py
@@ -1,7 +1,7 @@
 from operator import mod
 from unittest.util import _MAX_LENGTH
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Article(models.Model):
@@ -10,9 +10,11 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='images/',blank=True)
-    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+                            
 class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
--- Django/댓글/articles/views.py
+++ Django/댓글/articles/views.py
@@ -5,13 +5,17 @@ import accounts
 from .models import Article, Comment
 from .form import ArticleForm,CommentForm
 from django.contrib import messages
+from django.core.paginator import Paginator
+
 # Create your views here.
 def index(request):
     articles = Article.objects.order_by('-pk')
-    context={
+    
+    context={    
         'articles' : articles
     }
     return render(request,'articles/index.html',context)
+
 
 # def new(request):
 #     article_form = ArticleForm()
@@ -24,7 +28,10 @@ def create(request):
     if request.method == 'POST':
         article_form = ArticleForm(request.POST, request.FILES)
         if article_form.is_valid():
-            article_form.save()
+            article = article_form.save(commit=False)
+            # 로그인한 유저 => 작성자네!
+            article.user = request.user 
+            article.save()
             messages.success(request, '글 작성이 완료되었습니다.')
             return redirect('articles:index')
     else: 
@@ -49,28 +56,41 @@ def detail(request, pk):
 @login_required
 def update(request,pk):
     article = Article.objects.get(pk=pk)
-    if request.method == 'POST':
-        article_form = ArticleForm(request.POST, instance=article)
-        if article_form.is_valid():
-            article_form.save()
-            return redirect('articles:detail', article.pk)
+    if request.user == article.user:
+        if request.method == 'POST':
+            article_form = ArticleForm(request.POST, instance=article)
+            if article_form.is_valid():
+                article_form.save()
+                messages.success(request, '글이 수정되었습니다.')
+                return redirect('articles:detail', article.pk)
+        else:
+            article_form = ArticleForm(instance=article)
+        context = {
+            'article_form' : article_form
+        }
+        return render(request,'articles/form.html', context)
     else:
-        article_form = ArticleForm(instance=article)
-    context = {
-        'article_form' : article_form
-    }
-    return render(request,'articles/form.html', context)
+        # 작성자가 아닐 때
+        # (1) 403 에러메시지를 던져버린다. 
+        # from django.http import HttpResponseForbidden
+        # return HttpResponseForbidden()
+        # (2) flash message 활용!
+        messages.warning(request, '작성자만 수정할 수 있습니다.')
+        return redirect('articles:detail', article.pk)
+
 
 def delete(request, pk):
     Article.objects.get(pk=pk).delete()
     return redirect('articles:index')
 
+@login_required
 def comment_create(request, pk):
     article = Article.objects.get(pk=pk)
     comment_form = CommentForm(request.POST)
     if comment_form.is_valid():
         comment = comment_form.save(commit=False)
         comment.article = article
+        comment.user = request.user
         comment.save()
     return redirect('articles:detail', article.pk)
 
